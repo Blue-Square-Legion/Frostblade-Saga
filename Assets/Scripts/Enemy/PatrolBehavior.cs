ff4d87bdddde5cc3c69e21f25b62971a6915b906
chiferito <ericwongliu@hotmail.com>
2024-08-22 12:26:48 -0500

    patrol

--- Assets/Scripts/Enemy/PatrolBehavior.cs
+++ Assets/Scripts/Enemy/PatrolBehavior.cs
@@ -62,7 +62,9 @@ public class PatrolBehavior : MonoBehaviour
     private void MoveInDirection(int _direction)
     {
         idleTimer = 0;
+        //flip enemy sprite to face the correct direction
         enemy.localScale = new Vector3(Mathf.Abs(initScale.x) * _direction, initScale.y, initScale.z);
+        //move enemy
         enemy.position = new Vector3(enemy.position.x + Time.deltaTime * _direction * speed,
             enemy.position.y, enemy.position.z);
     }
