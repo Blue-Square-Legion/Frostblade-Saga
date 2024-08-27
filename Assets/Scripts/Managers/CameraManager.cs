30757fe2e064d0ef0b85d120492d0f168f148346
chiferito <ericwongliu@hotmail.com>
2024-08-27 11:42:20 -0500

    Implemented level 1, fixed tile artifacts, added camera boundaries

--- Assets/Scripts/Managers/CameraManager.cs
+++ Assets/Scripts/Managers/CameraManager.cs
@@ -5,6 +5,11 @@ public class CameraManager : MonoBehaviour
     [SerializeField] private Transform player;
     [SerializeField] private AudioClip bgm;
 
+    [SerializeField] private float maxX;
+    [SerializeField] private float maxY;
+    [SerializeField] private float minX;
+    [SerializeField] private float minY;
+
     private void Start()
     {
         SoundManager.Instance.PlaySound(bgm);
@@ -13,6 +18,7 @@ public class CameraManager : MonoBehaviour
     private void Update()
     {
         //Follow player
-        transform.position = new Vector3(player.position.x, player.position.y, transform.position.z); 
+        
+        transform.position = new Vector3(Mathf.Clamp(player.position.x, minX, maxX), Mathf.Clamp(player.position.y, minY, maxY), transform.position.z); 
     }
 }
