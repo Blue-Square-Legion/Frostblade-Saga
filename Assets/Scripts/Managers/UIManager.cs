c79841507fcf6873ccf4ae571445a72fc1c4a25d
chiferito <ericwongliu@hotmail.com>
2024-08-31 12:55:22 -0500

    init

--- Assets/Scripts/Managers/UIManager.cs
+++ Assets/Scripts/Managers/UIManager.cs
@@ -1,32 +1,45 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
-    #region Singleton
-    private static UIManager instance;
-    public static UIManager Instance
+    [SerializeField] GameObject gameOverScreen;
+    [SerializeField] GameObject creditsScreen;
+    [SerializeField] GameObject healthBar;
+    [SerializeField] AudioClip gameOverSound;
+
+
+    [SerializeField] GameObject pauseScreen;
+
+    private void Start()
     {
-        get
-        {
-            if (instance == null)
-                instance = FindObjectOfType(typeof(UIManager)) as UIManager;
-            return instance;
-        }
-        set
-        {
-            instance = value;
-        }
-    }
-    #endregion
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+        healthBar.SetActive(true);
+        gameOverScreen.SetActive(false);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void GameOver()
     {
-        
+        healthBar.SetActive(false);
+        gameOverScreen.SetActive(true);
+        SoundManager.Instance.PlaySound(gameOverSound);
     }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void Play()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void Credits()
+    {
+        creditsScreen.SetActive(true);
+    }
+
 }
