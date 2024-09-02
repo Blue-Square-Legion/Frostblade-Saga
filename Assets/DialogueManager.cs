98cbbd1908f323dac14b8d329a8370b77ce09275
iVinzzzz <vinz.dizon1@gmail.com>
2024-09-03 03:48:48 +0800

    Added Tutorial Scene

---
+++ Assets/DialogueManager.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+using TMPro;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class DialogueManager : MonoBehaviour
+{
+    public static DialogueManager Instance;
+
+    
+    public TextMeshProUGUI characterName;
+    public TextMeshProUGUI dialogueArea;
+
+    private Queue<DialogueLine> lines;
+
+    public bool isDialogueActive = false;
+
+    public float typingSpeed = 0.2f;
+
+    public Animator animator;
+
+    private void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+
+        lines = new Queue<DialogueLine>();
+    }
+
+    public void StartDialogue(Dialogue dialogue)
+    {
+        isDialogueActive = true;
+
+        animator.Play("PopUpText");
+
+        lines.Clear();
+
+        foreach (DialogueLine dialogueLine in dialogue.dialogueLines)
+        {
+            lines.Enqueue(dialogueLine);
+        }
+
+        DisplayNextDialogueLine();
+    }
+
+    public void DisplayNextDialogueLine()
+    {
+        if (lines.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        DialogueLine currentLine = lines.Dequeue();
+
+        
+        characterName.text = currentLine.character.name;
+
+        StopAllCoroutines();
+
+        StartCoroutine(TypeSentence(currentLine));
+    }
+
+    IEnumerator TypeSentence(DialogueLine dialogueLine)
+    {
+        dialogueArea.text = "";
+        foreach (char letter in dialogueLine.line.ToCharArray())
+        {
+            dialogueArea.text += letter;
+            yield return new WaitForSeconds(typingSpeed);
+        }
+    }
+
+    void EndDialogue()
+    {
+        isDialogueActive = false;
+        animator.Play("Hide");
+        EventSystem.current.SetSelectedGameObject(null);
+        SceneManager.LoadScene(2);
+    }
+}
---
+++ Assets/DialogueTrigger.cs
@@ -0,0 +1,45 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class DialogueCharacter
+{
+    public string name;
+    public Sprite icon;
+}
+
+[System.Serializable]
+public class DialogueLine
+{
+    public DialogueCharacter character;
+    [TextArea(3, 10)]
+    public string line;
+}
+
+[System.Serializable]
+public class Dialogue
+{
+    public List<DialogueLine> dialogueLines = new List<DialogueLine>();
+}
+
+public class DialogueTrigger : MonoBehaviour
+{
+    public Dialogue dialogue;
+   
+
+    public void TriggerDialogue()
+    {
+        DialogueManager.Instance.StartDialogue(dialogue);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            TriggerDialogue();
+            GetComponent<Collider2D>().enabled = false;
+        }
+    }
+
+
+}
