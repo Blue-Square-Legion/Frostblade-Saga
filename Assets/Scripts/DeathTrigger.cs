68d587e894752f42437135ac458efca4e5f35e87
chiferito <ericwongliu@hotmail.com>
2024-09-06 11:59:58 -0500

    Enemies and camera positions

--- Assets/Scripts/DeathTrigger.cs
+++ Assets/Scripts/DeathTrigger.cs
@@ -2,12 +2,11 @@ using UnityEngine;
 
 public class DeathTrigger : MonoBehaviour
 {
-    [SerializeField] Checkpoint checkPoint;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
-            checkPoint.CheckpointRespawn();
+            UIManager.Instance.GameOver();
         }
     }
 }
